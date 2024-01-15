fb79fc09bc224f68bdec247fe12f577397ebb8c7
Guo-sen <2410914566@qq.com>
2024-01-15 08:14:13 +0800

    学习更新

--- 练习题/test.c
+++ 练习题/test.c
@@ -5,6 +5,52 @@
 #include<windows.h>
 #include<math.h>
 
+//编写代码 使得三个数按从大到小输出
+
+//int main()
+//{
+//	int a = 0, b = 0, c = 0;
+//	int tem = 0;
+//	scanf("%d %d %d",&a,&b,&c);
+//	if (a < b)
+//	{
+//		tem = a;
+//		a = b;
+//		b = tem;
+//	}
+//	if (a < c)
+//	{
+//		tem = a;
+//		a = c;
+//		c = tem;
+//	}
+//	if (b < c)
+//	{
+//		tem = c;
+//		c = b;
+//		b = tem;
+//	}
+//	printf("%d %d %d \n", a, b, c);
+//	return 0;
+//}
+
+//写一个代码 打印1~100间所有的3的倍数
+//int main()
+//{
+//	int i = 0;
+//	/*for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 3 == 0)
+//		{
+//			printf("%d ",i);
+//		}
+//	}*/
+//	for (i = 3; i <= 100; i+=3)
+//	{
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
 
 //计算n的阶乘
 //int main()
@@ -302,6 +348,157 @@
 //	return 0;
 //}
 
+//练习：接收一个整型值（无符号），按照顺序打印它的每一位(p.46)
+//print(1234)  123  4(1234%10)
+//print(123)   12   3(123%10)
+//print(12)    1    2(12%10)
+//
+//void print(unsigned int num)
+//{
+//	if (num > 9)
+//	{
+//		print(num/10);
+//	}
+//	printf("%d ",num%10);
+//
+//}
+//int main()
+//{
+//	unsigned int a = 0;
+//	scanf("%d",&a);
+//	print(a);
+//	return 0;
+//}
+
+
+
+
+
+//模拟实现strlen
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str!='\0')
+//	{
+//			count++;
+//			str++;
+//	}
+//	return count;
+//}
+
+//练习 编写函数 不允许创建临时变量求字符串的长度
+////"abcd"，如果*str不是'\0',那字符长度至少是1，可以转换成求 1+my_strlen(bcd)
+//int my_strlen(char* str)
+//{
+//	if (*str != '\0')
+//	{
+//		return (1 + my_strlen(str + 1));//不推荐写++str，str+1不会改变str本身，++str把str本身也给改变了	
+//	}
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	int len= my_strlen("abcd");//字符串传送的时候传的是字符串的起始地址，即a的地址
+//	printf("len=%d",len);
+//	return 0;
+//}
+
+//写一个函数计算n的阶乘
+
+//递归的方式
+//int fac(int n)
+//{
+//	 if (n <= 1)
+//	 {
+//		return 1;
+//	 }
+//
+//	else if (n > 1)
+//	 {
+//		return n*fac(n-1);
+//	 }
+//}
+
+//迭代的方式--非递归(类似于循环)
+
+//int fac(int n)
+//{
+//	int i = 0;
+//	int ret = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret = ret*i;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d",&num);
+//	int ret = fac(num);
+//	printf("num!= %d\n",ret);
+//
+//	return 0;
+//}
+
+//求第n给斐波那契数（不考虑溢出）
+//Fib(n): n<=2    1
+//		  n>2     Fib(n-1)+Fib(n-2)
+
+
+//int count = 0;
+//int Fib(int n)
+//{
+//	if (n == 3)//计算一下在整个计算过程中n等于3计算过多少次
+//			   //计算第40个斐波那契数 n=3计算了  39088169次
+//	{
+//		count++;
+//	}
+//	if (n <= 2)
+//	{
+//		return 1;
+//	}
+//	else if(n>2)
+//	{
+//		return (Fib(n - 1) + Fib(n - 2));//return Fib(n - 1) + Fib(n - 2);
+//	}
+//}
+//以上函数效率太低
+//40
+//39       38
+//38      37    37     36
+//37 36  36 35 36 35  35 34
+
+//优化
+
+//int Fib(int n)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n >= 3)
+//	{
+//	
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	printf("请输入n的值： ");
+//	scanf("%d",&n);
+//	int ret = Fib(n);
+//	printf("第%d个斐波那契数是：%d\n",n,ret);
+//	//printf("%d\n",count);
+//	return 0;
+//}
+
 
  //写一个函数，实现一个整型有序数组的二分查找
 //int binary_search(int arr[],int k,int sz)//找到了返回下标，找不到返回-1
@@ -345,28 +542,306 @@
 // 
 // 
 // 
+//思考以下代码的打印结果,printf的返回值是打印的字符个数
 
+//int main()
+//{
+//	printf("%d",printf("%d",printf("%d",43)));
+//	return 0;
+//}
+////4321
 
 
 
 
 //写一个函数，每次调用这个函数，就会将num的值+1；
-void Add(int* p)
-{
-	*p += 1;
-	//*p++;
-}
-int main()
-{
-	int num = 0;
-	Add(&num);
-	printf("%d ", num);
-	Add(&num);
-	printf("%d ", num);
-	Add(&num);
-	printf("%d ", num);
-	return 0;
-}
+//void Add(int* p)
+//{
+//	*p += 1;
+//	//*p++;
+//}
+//int main()
+//{
+//	int num = 0;
+//	Add(&num);
+//	printf("%d ", num);
+//	Add(&num);
+//	printf("%d ", num);
+//	Add(&num);
+//	printf("%d ", num);
+//	return 0;
+//}
+
+//练习：接收一个整型值（无符号），按照顺序打印它的每一位(p.46)
+//print(1234)  123  4(1234%10)
+//print(123)   12   3(123%10)
+//print(12)    1    2(12%10)
+//
+//void print(unsigned int num)
+//{
+//	if (num > 9)
+//	{
+//		print(num/10);
+//	}
+//	printf("%d ",num%10);
+//
+//}
+//int main()
+//{
+//	unsigned int a = 0;
+//	scanf("%d",&a);
+//	print(a);
+//	return 0;
+//}
+
+
+
+
+
+//模拟实现strlen
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str!='\0')
+//	{
+//			count++;
+//			str++;
+//	}
+//	return count;
+//}
+
+//练习 编写函数 不允许创建临时变量求字符串的长度
+////"abcd"，如果*str不是'\0',那字符长度至少是1，可以转换成求 1+my_strlen(bcd)
+//int my_strlen(char* str)
+//{
+//	if (*str != '\0')
+//	{
+//		return (1 + my_strlen(str + 1));//不推荐写++str，str+1不会改变str本身，++str把str本身也给改变了	
+//	}
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	int len= my_strlen("abcd");//字符串传送的时候传的是字符串的起始地址，即a的地址
+//	printf("len=%d",len);
+//	return 0;
+//}
+
+//写一个函数计算n的阶乘
+
+//递归的方式
+//int fac(int n)
+//{
+//	 if (n <= 1)
+//	 {
+//		return 1;
+//	 }
+//
+//	else if (n > 1)
+//	 {
+//		return n*fac(n-1);
+//	 }
+//}
+
+//迭代的方式--非递归(类似于循环)
+
+//int fac(int n)
+//{
+//	int i = 0;
+//	int ret = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret = ret*i;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d",&num);
+//	int ret = fac(num);
+//	printf("num!= %d\n",ret);
+//
+//	return 0;
+//}
+
+//求第n给斐波那契数（不考虑溢出）
+//Fib(n): n<=2    1
+//		  n>2     Fib(n-1)+Fib(n-2)
+
+
+//int count = 0;
+//int Fib(int n)
+//{
+//	if (n == 3)//计算一下在整个计算过程中n等于3计算过多少次
+//			   //计算第40个斐波那契数 n=3计算了  39088169次
+//	{
+//		count++;
+//	}
+//	if (n <= 2)
+//	{
+//		return 1;
+//	}
+//	else if(n>2)
+//	{
+//		return (Fib(n - 1) + Fib(n - 2));//return Fib(n - 1) + Fib(n - 2);
+//	}
+//}
+//以上函数效率太低
+//40
+//39       38
+//38      37    37     36
+//37 36  36 35 36 35  35 34
+
+//优化
+
+//int Fib(int n)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n >= 3)
+//	{
+//	
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	printf("请输入n的值： ");
+//	scanf("%d",&n);
+//	int ret = Fib(n);
+//	printf("第%d个斐波那契数是：%d\n",n,ret);
+//	//printf("%d\n",count);
+//	return 0;
+//}
+
+//int main()
+//{
+//
+//	return 0;
+//}
+
+
+//给定两个数，求最大公约数
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	printf("请输入两个数：");
+//	scanf("%d %d",&a,&b);
+//	//思路：从两个数中找出较小的一个，然后看看能否同时整除它俩，不能则减一，
+//	//再看是否能整除，不能再减一，查看是否能整除
+//	//int min = (a > b ? b : a);
+//	//int m = min;
+//	//while (1)
+//	//{
+//	//	if (a % m == 0 && b % m == 0)
+//	//	{
+//	//		break;
+//	//	}
+//	//	m--;
+//	//}
+//
+//	//辗转相除法
+//	int c = 0;
+//	while (c=a % b)
+//	{
+//		a = b;
+//		b = c;
+//	}
+//	printf("%d\n",b);
+//	return 0;
+//}
+
+//编写代码，数一下1~100的整数中出现过多少次数字9
+
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		
+//		//判断个位是不是9
+//		if (i%10==9)
+//		{
+//			count++;
+//		}
+//		//判断10位是不是9
+//		if (i / 10 == 9)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("%d ",count);
+//
+//	return 0;
+//}
+
+
+//计算1/1-1/2+1/3-1/4+1/5.....+1/99-1/100
+
+//分子都是1
+//分母是1~100
+
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0;
+//	int flag = 1;
+//
+//	for (i = 1; i <= 100; i++)
+//	{
+//		sum = sum + flag*(1.0 / i);
+//		flag = -flag;
+//	}
+//	printf("%lf ",sum);
+//	return 0;
+//}
+
+
+//求10个整数中的最大值
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int max = arr[0];
+//	int i = 0;
+//	for (i=1;i<10;i++)
+//	{
+//		if (arr[i]>max)
+//		{
+//			max=arr[i];
+//		}
+//	}
+//	printf("max=%d",max);
+//	return 0;
+//}
+
+//打印9*9乘法口诀表
+//int main()
+//{
+//	int i = 0;//控制行
+//	int j = 0;//控制每行有几列
+//	for (i = 1; i <= 9; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%-2d ",j,i,i*j);//%-2d 两位左对齐
+//										//%2d 两位右对齐
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
 
 
 
