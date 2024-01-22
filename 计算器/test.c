5fae86dde52bcae2b5089e24fe73011f7a9e32c9
Guo-sen <2410914566@qq.com>
2024-01-22 09:42:35 +0800

    更新代码

--- 计算器/test.c
+++ 计算器/test.c
@@ -32,39 +32,71 @@ void calc(int(*pf)(int, int))
 	int ret = (*pf)(x, y);
 	printf("%d\n",ret);
 }
+//int main()
+//{
+//	int input = 0;
+//
+//	do
+//	{
+//		menu();
+//		printf("请输入你的选择:> ");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			calc(Add);
+//			break;
+//		case 2:
+//			calc(Sub);
+//				break;
+//		case 3:
+//			calc(Mul);
+//			break;
+//		case 4:
+//			calc(Div);
+//			break;
+//		case 0:
+//			printf("退出程序\n");
+//			break;
+//		default:
+//			printf("输入错误，请重新输入\n");
+//			break;
+//		}
+//	} while (input);
+//
+//	return 0;
+//}
+//以上代码如果不使用函数指针，就只能把calc函数的内容写在switch的case里，大大减少了代码冗余
+//（写在switch上边退出逻辑也有问题，退出时还得输入两个操作数）
+
+
+
+
 int main()
 {
 	int input = 0;
+	int(*pf[5])(int, int) = { 0,Add,Sub,Mul,Div };
+	int x = 0, y = 0;
 
 	do
 	{
 		menu();
 		printf("请输入你的选择:> ");
 		scanf("%d", &input);
-		switch (input)
+		if (input == 0)
 		{
-		case 1:
-			calc(Add);
-			break;
-		case 2:
-			calc(Sub);
-				break;
-		case 3:
-			calc(Mul);
-			break;
-		case 4:
-			calc(Div);
-			break;
-		case 0:
-			printf("退出程序\n");
-			break;
-		default:
-			printf("输入错误，请重新输入\n");
+			printf("退出\n");
 			break;
 		}
+		else if(input>=1&&input<=4)
+		{
+			printf("请输入两个操作数：");
+			scanf("%d %d", &x, &y);
+			int ret=pf[input](x, y);
+			printf("%d\n", ret);
+		}
 	} while (input);
-
 	return 0;
 }
-//以上代码如果不使用函数指针，就只能把calc函数的内容写在switch的case里，大大减少了代码冗余
-//（写在switch上边退出逻辑也有问题，退出时还得输入两个操作数）
+//使用函数指针数组，可以不用switch的方式来完成选择（当功能很多时，case会变得很多），缩短了代码，使得代码易于维护
+
