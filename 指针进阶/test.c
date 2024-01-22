4fab0a582b8f9fa07711bca292195e7c28ecd77f
Guo-sen <2410914566@qq.com>
2024-01-22 20:51:52 +0800

    更新

--- 指针进阶/test.c
+++ 指针进阶/test.c
@@ -288,13 +288,217 @@
 //	return 0;
 //}
 
-//函数指针数组
+//函数指针数组 把函数指针放在数组里，这个数组就是函数指针数组(函数指针数组又叫转移表)
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+//
+//
+//int main()
+//{
+//	int(*pf)(int, int) = Add;
+//	int (*arr[5])(int, int) = { Add,Sub,Mul,Div };//p先和[4]结合，说明这是个数组，去掉数组名，剩下int (*)(int,int),这是每个元素的类型
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%d\n",arr[i](4,8));
+//	}
+//	return 0;
+//}
+//用途参考计算器的第二个版本	
 
 
 
+//指向函数指针数组的指针
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+//
+//
+//int main()
+//{
+//	//函数指针数组
+//	int (*arr[5])(int, int) = { Add,Sub,Mul,Div };
+//
+//	//指向函数指针数组的指针
+//	int (*(*parr)[5])(int, int) = &arr;//*parr说明这是一个指针，指向一个数组，有五个元素，每个元素的类型是函数指针int (*)(int,int)
+//
+//	return 0;
+//}
+
+
+//void* 类型指针
+//int main()
+//{
+//	int a = 10;
+//	char* pc = &a;//会有警告
+//	void* pv = &a;//void* 是无具体类型的指针，可以接受任意类型的地址，不会有警告
+//	//void* 是无具体类型的指针，所以不能直接解引用操作，也不能+-整数，使用前需要强制类型转换
+//	return 0;
+//}
+
+//回调函数：回调函数就是一个通过函数指针调用的函数。如果把函数指针作为参数传递给另一个函数，当这个指针被用来调用其所指向的函数时，
+//我们就说这是回调函数。	
 
 
 
+// qsort函数：void qsort( void *base,//要排序的数据的起始位置 
+//						size_t num, //待排序的数据元素的个数
+//						size_t width, //待排序的数据元素的大小（单位是字节）
+//						int (*compare )(const void *elem1, const void *elem2 ) );//函数指针--比较函数
+// 对compare函数返回值的规定  
+//   <0    elem1 less than elem2 
+//   0     elem1 equivalent to elem2
+//   >0    elem1 greater than elem2
+
+
+//比较2个整型
+//e1指向一个整数
+//e2指向另外一个整数	
+//int com_int(const void* e1, const void* e2)
+//{
+//	return (*(int*)e1 - *(int*)e2);//qsort调用比较函数时，默认排升序，如果需要排降序，把e1和e2调换位置
+//}
+//int main()
+//{
+//	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	qsort(arr, sz,sizeof(arr[0]), com_int);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+//使用qsort排序结构体
+#include<string.h>
+#include<stdlib.h>
+#include<search.h>
+
+
+//struct Stu
+//{
+//	char name[20];
+//	int age;
+//};
+//
+//int com_by_name(const void* e1, const void* e2)
+//{
+//	//strcmp返回值恰好是>0  ==0  <0
+//	return strcmp(((struct Stu*)e1)->name,((struct Stu*)e2)->name);
+//}
+//int com_by_age(const void* e1, const void* e2)
+//{
+//	//strcmp返回值恰好是>0  ==0  <0
+//	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
+//}
+//int main()
+//{
+//	struct Stu s[] = { {"张三",18},{"李四",16},{"王五",19} };
+//	int sz = sizeof(s) / sizeof(s[0]);
+//	//qsort(s,sz,sizeof(s[0]), com_by_name);
+//	qsort(s, sz, sizeof(s[0]),com_by_age);
+//
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%s  %d  \n", s[i].name, s[i].age);
+//	}
+//	return 0;
+//}
+
+//对以前的冒泡排序的改造(p 113)
+struct Stu
+{
+	char name[20];
+	int age;
+};
+int com_by_age(const void* e1, const void* e2)
+{
+	//strcmp返回值恰好是>0  ==0  <0
+	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
+}
+
+void Swap(char* buff1,char*buff2,int width)
+{
+	int i = 0;
+	for (i = 0; i < width; i++)
+	{
+		char tmp = *buff1;
+		*buff1 = *buff2;
+		*buff2 = tmp;
+		buff1++;
+		buff2++;
+	}
+}
+void bubble_sort (void* arr, int sz, int width, int (*cmp)(const void* e1, const void* e2))
+{
+	int i = 0;
+	//趟数
+	for (i = 0; i < sz; i++)
+	{
+		int flag = 1;
+		int j = 0;
+		//控制每趟排序排多少对
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			if (cmp((char*)arr + j * width, (char*)arr + (j + 1) * width)>0)
+			{
+				//交换
+				Swap((char*)arr + j * width, (char*)arr + (j + 1) * width,width);
+				flag = 0;
+			}
+		
+		}
+		if (flag == 0)
+		{
+			break;
+		}
+	}
+	
+}
+int main()
+{
+	struct Stu s[] = { {"张三",18},{"李四",16},{"王五",19} };
+	int sz = sizeof(s) / sizeof(s[0]);
+	//qsort(s,sz,sizeof(s[0]), com_by_name);
+	bubble_sort(s, sz, sizeof(s[0]),com_by_age);
+
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%s  %d  \n", s[i].name, s[i].age);
+	}
+	return 0;
+}
 
 
 
